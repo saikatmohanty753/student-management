9e78b67be75859da7f8dbc9db6ad7bff481c1d9f
santoshbbsr <santoshchoudhury5@gmail.com>
2023-01-18 12:12:31 +0530

    push

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\College;
+use App\Models\CollegeAffiliation;
+use App\Models\Course;
+use App\Models\RenewalAffiliation;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+
+class DashboardController extends Controller
+{
+    public function dashboard()
+    {
+
+        $chk_role = $this->checkUser(Auth::user()->role_id);
+        if ($chk_role == 0) {
+            Auth::logout();
+            return redirect('/login')->with('error', 'The username and password you entered did not match our records');
+        }
+        if (Str::lower(Auth::user()->role->name) == 'academic-section') {
+            return $this->academicSection();
+        } elseif (Str::lower(Auth::user()->role->name) == 'student-portal') {
+            return $this->adminDashboard();
+        } elseif (Str::lower(Auth::user()->role->name) == 'exam-section') {
+            return $this->examSection();
+        } elseif (Str::lower(Auth::user()->role->name) == 'college-exam-section') {
+            return $this->collegeExamSection();
+        } elseif (Str::lower(Auth::user()->role->name) == 'college-academic-section') {
+            return $this->collegeAcademicSection();
+        } elseif (Str::lower(Auth::user()->role->name) == 'student') {
+            return $this->studentDashboard();
+        } else {
+            Auth::logout();
+            return redirect('/login')->with('error', 'The username and password you entered did not match our records');
+        }
+    }
+
+    public function checkUser($id)
+    {
+        $role = [ 3, 9, 10, 11, 12, 13, 14];
+        return in_array($id, $role) == true ? 1 : 0;
+    }
+
+    public function adminDashboard()
+    {
+        return view('dashboard.admin.index');
+    }
+    public function academicSection()
+    {
+        return view('dashboard.academic.index');
+    }
+    public function examSection()
+    {
+        return view('dashboard.examination.index');
+    }
+    public function studentDashboard()
+    {
+        return view('dashboard.student.index');
+    }
+    public function collegeAcademicSection()
+    {
+        return view('dashboard.college-academic.index');
+    }
+    public function collegeExamSection()
+    {
+        return view('dashboard.college-examination.index');
+    }
+}
---
+++ app/Http/Controllers/NoticesController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Http\Request;
+use App\Models\Course;
+use App\Models\CourseFor;
+use App\Models\Notice;
+use Carbon\Carbon;
+
+class NoticesController extends Controller
+{
+    function __construct()
+
+    {
+
+        $this->middleware('permission:notice-list|notice-create|notice-edit|notice-delete', ['only' => ['index', 'store']]);
+
+        $this->middleware('permission:notice-create', ['only' => ['create', 'store']]);
+
+        $this->middleware('permission:notice-edit', ['only' => ['edit', 'update']]);
+
+        $this->middleware('permission:notice-delete', ['only' => ['destroy']]);
+    }
+    public function index()
+    {
+        $notice = Notice::all();
+        return view('notices.notices', compact('notice'));
+    }
+
+    public function create()
+    {
+        $course = Course::all();
+        $dept = CourseFor::all();
+        return view('notices.add_notice', compact('course', 'dept'));
+    }
+
+    public function store(Request $request)
+    {
+        return back()->with('success', 'Notification Created Successfully');
+        $startDate = Carbon::parse($request->start_date);
+        $startDate->hour   = 00;
+        $startDate->minute = 00;
+        $startDate->second = 01;
+        $expDate = Carbon::parse($request->exp_date);
+        $expDate->hour   = 59;
+        $expDate->minute = 59;
+        $expDate->second = 59;
+        $notice = new Notice();
+        $notice->notice_type = $request->notice_type;
+        $notice->department_id = $request->department;
+        $notice->course_id = $request->course != '' ? implode(',', $request->course) : '';
+        $notice->semester = $request->semester;
+        $notice->start_date = $startDate;
+        $notice->exp_date = $expDate;
+        $notice->details = $request->details;
+        $notice->save();
+        return redirect()->action([NoticesController::class, 'index'])->with('success', 'Notification Created Successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
