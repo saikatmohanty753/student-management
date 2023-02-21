987e7a15b4bc2f0b34a416b367c3bf7c66118f18
soumya <sahoosoumyaranjan29@gmail.com>
2023-02-21 16:39:46 +0530

    soumya

---
+++ app/Http/Controllers/CreditController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Credit;
+
+class CreditController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $credit = Credit::all();
+        return view('credit.index',compact('credit'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $credit = new Credit();
+        $credit->credit = $request->credit;
+        $credit->save();
+        return redirect('/credit');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $credit = Credit::whereIn('id', $id)->get();
+        return view('course_master.index', compact('credit'));
+        return redirect()->back();
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $credit = Credit::find($id);
+        $credit->credit = $request->credit;
+        $credit->save();
+        return redirect('/credit');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $credit = Credit::find($id);
+        $credit->delete();
+        return redirect('/credit');
+    }
+}
