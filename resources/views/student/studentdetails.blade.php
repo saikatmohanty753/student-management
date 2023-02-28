5d8741a296657aca26846bc5b3f855f7aebe9700
soumya <sahoosoumyaranjan29@gmail.com>
2023-02-28 12:46:05 +0530

    changes

---
+++ resources/views/student/studentdetails.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+@section('content')
+{{-- @php
+  dd($CollegeName);  
+@endphp --}}
+
+<div class="container">
+    <div class="row">
+
+        <div class="col-md-4">
+           
+            </div>
+
+            
+            </div>
+
+
+        </div>
+
+
+
+        
+
+
+
+
+@endsection
